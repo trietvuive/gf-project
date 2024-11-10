6bc4698650d1d6d6e197efefc1bca01b508dfa4b
trietvuive <vominhtriet02@gmail.com>
2024-11-09 22:39:46 -0800

    forgot files

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import ResponseButton from "../components/ResponseButton/ResponseButton";
+
+export default function Home() {
+  return (
+    <div>
+      <ResponseButton/>
+    </div>
+  );
+}
